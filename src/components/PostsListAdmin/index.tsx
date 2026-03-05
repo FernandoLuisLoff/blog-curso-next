d02f8a26e41dbdcc23eb28c064a29d39adff9167
Fernando Loff <fernandoloff@fazendamelhor.com.br>
2026-03-05 08:20:51 -0300

    delete post

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,13 +1,37 @@
+import { deletePostAction } from "@/actions/delete-post";
 import { findAllPostAdmin } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import { Trash2Icon } from "lucide-react";
+import Link from "next/link";
+import { DeletePostButton } from "../admin/DeletePostButton";
 
 export default async function PostsListAdmin() {
     const posts = await findAllPostAdmin();
 
     return (
-        <div className='py-16'>
-        {posts.map(post => {
-            return <p key={post.id}>{post.title}</p>;
-        })}
+        <div className='mb-16'>
+            {posts.map(post => {
+                return (
+                    <div key={post.id} 
+                        className={clsx(
+                            'py-2 px-2',
+                            !post.published && 'bg-slate-300',
+                            'flex gap-2 items-center justify-between'
+                        )}
+                    >
+                        <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+                        {!post.published && (
+                            <span className="text-xs text-slate-600 italic">
+                                (Não publicado)
+                            </span>
+                        )}
+                        <DeletePostButton 
+                            id={post.id}
+                            title={post.title}
+                        />
+                    </div>
+                );
+            })}
         </div>
     );
 }
