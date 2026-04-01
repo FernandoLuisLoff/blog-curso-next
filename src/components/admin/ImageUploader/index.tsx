047d882b220d7f4c2e5f738ffb7fd2c9d3217383
Fernando Loff <fernandoloff@fazendamelhor.com.br>
2026-04-01 17:35:29 -0300

    Image uploader

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef } from "react";
+import { useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
     const fileInputRef = useRef<HTMLInputElement>(null);
+    const [isUploading, startTransition] = useTransition();
+    const [imgUrl, setImgUrl] = useState('');
 
     function handleChooseFile() {
         if (!fileInputRef.current) return;
@@ -15,22 +18,44 @@ export function ImageUploader() {
     }
 
     function handleChange() {
-        if (!fileInputRef.current) return;
+        if (!fileInputRef.current) {
+            setImgUrl('');
+            return;
+        }
 
         const fileInput = fileInputRef.current; 
         const file = fileInput?.files?.[0];
 
-        if (!file) return;
+        if (!file) {
+            setImgUrl('');
+            return;
+        }
 
         if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
             const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
             toast.error(`Imagem muito grande. Máx.: ${readableMaxSize}KB.`);
             fileInput.value = '';
+            setImgUrl('');
             return;
         }
 
         const formData = new FormData();
         formData.append('file', file);
+
+        startTransition(async () => {
+            const result = await uploadImageAction(formData);
+
+            if (result.error) {
+                toast.error(result.error);
+                fileInput.value = '';
+                setImgUrl('');
+                return;
+            }
+
+            setImgUrl(result.url);
+            
+            toast.success('Imagem enviada');
+        });
 
         fileInput.value = '';
     }
@@ -41,10 +66,19 @@ export function ImageUploader() {
                 type="button"
                 className="self-start"
                 onClick={handleChooseFile}
+                disabled={isUploading}
             >
                 <ImageUpIcon />
                 Enviar uma imagem
             </Button>
+
+            {!!imgUrl && (
+                <div className="flex flex-col gap-4">
+                    <p><b>URL:</b> {imgUrl}</p>
+                    <img className="rounded-lg" src={imgUrl} />
+                </div>
+            )}
+
             <input
                 ref={fileInputRef}
                 name="file"
@@ -52,6 +86,7 @@ export function ImageUploader() {
                 className="hidden"
                 accept="image/*"
                 onChange={handleChange}
+                disabled={isUploading}
             />
         </div>
     )
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,3 @@
 export const IMAGE_UPLOAD_MAX_SIZE = 921600; // 900 KB
+export const IMAGE_UPLOAD_DIRECTORY = 'uploads'; // Directory for uploaded images
+export const IMAGE_SERVER_URL = 'http://localhost:3000/uploads'; 
